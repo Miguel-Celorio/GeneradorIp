5da44cd52c33ed247f72f511445043dfdebd48aa
Miguel <miguel.celorio.09@gmail.com>
2022-03-29 21:37:55 +0200

    execute test 1000 times

--- src/GeneradorIpTest.java
+++ src/GeneradorIpTest.java
@@ -11,13 +11,19 @@ class GeneradorIpTest {
 	void testGenerarNumero() {
 		int numero = g.generarNumero(0, 255);
 		assertTrue(numero >= 0 && numero <= 254);
-		
-		
 	}
 	
 	@Test
 	void testGenerarIp() {
-		String ip = g.generarIp();
-		assertTrue(ip.charAt(0) != 0 && ip.charAt(ip.length()-1) != 0);
+		boolean flag = true;
+		for (int i = 0; i < 1000; i++){
+			String ip = g.generarIp();
+			if(!(ip.charAt(0) != 0 && ip.charAt(ip.length()-1) != 0)){
+				flag = false;
+				break;
+			}
+		}
+
+		assertTrue(flag);
 	}
 }
